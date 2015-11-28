11fb434a370cbc4938a6bb31bd47b2b73b1f52a3
Yu-Ting, Kao <jackzzjack@gmail.com>
2015-11-29 03:13:47 +0800

    Add build script.

---
+++ build_YuTingConfig.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# For instance toolchain and linux of raspberry PI are at same folder.
+export PATH=../tools/arm-bcm2708/gcc-linaro-arm-linux-gnueabihf-raspbian/bin/:$PATH
+
+KERNEL=kernel7
+
+# Copy default configure setting @ .config
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- bcm2709_defconfig
+# make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- menuconfig
+
+cp YuTingConfig.config .config
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- zImage modules dtbs -j6
